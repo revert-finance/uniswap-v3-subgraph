9cdabe3f688b48f61cda9eb73daf88dce1ccb44b
Dan Li <dianwen@gmail.com>
2023-04-07 13:16:36 -0600

    feat: strip out all but calls

---
+++ src/mappings/indexing_util.ts
@@ -0,0 +1,7 @@
+import { BigInt } from '@graphprotocol/graph-ts'
+
+export function shouldCacheCalls(blockNumber: BigInt): boolean {
+  // The subgraph has already indexed up to this block number, so no need to cache calls
+  // for blocks before this.
+  return blockNumber.gt(BigInt.fromI32(4991331))
+}
