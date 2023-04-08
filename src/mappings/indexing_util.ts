4ee97f69005999eada6e6a17b575a91a58252dc5
Dan Li <dianwen@gmail.com>
2023-04-07 19:40:45 -0600

    chore: bump up block number

--- src/mappings/indexing_util.ts
+++ src/mappings/indexing_util.ts
@@ -3,5 +3,5 @@ import { BigInt } from '@graphprotocol/graph-ts'
 export function shouldCacheCalls(blockNumber: BigInt): boolean {
   // The subgraph has already indexed up to this block number, so no need to cache calls
   // for blocks before this.
-  return blockNumber.gt(BigInt.fromI32(5086450))
+  return blockNumber.gt(BigInt.fromI32(5215174))
 }
